cf7705d70771500172b1bc0afba925ef91cc219e
lalitha2407 <lalitha@bootlabstech.com>
2023-04-04 16:30:09 +0530

    disk block removed

--- variables.tf
+++ variables.tf
@@ -1,30 +1,30 @@
-variable "resource_group_name" {
-  type        = string
-  description = "name of the resource group"
-}
+# variable "resource_group_name" {
+#   type        = string
+#   description = "name of the resource group"
+# }
 
-variable "location" {
-  type        = string
-  description = "location of the resource group"
-}
+# variable "location" {
+#   type        = string
+#   description = "location of the resource group"
+# }
 
-variable "storage_account_type" {
-  type        = string
-  description = "The type of storage to use for the managed disk. Possible values are Standard_LRS, StandardSSD_ZRS, Premium_LRS, PremiumV2_LRS, Premium_ZRS, StandardSSD_LRS or UltraSSD_LRS."
-}
+# variable "storage_account_type" {
+#   type        = string
+#   description = "The type of storage to use for the managed disk. Possible values are Standard_LRS, StandardSSD_ZRS, Premium_LRS, PremiumV2_LRS, Premium_ZRS, StandardSSD_LRS or UltraSSD_LRS."
+# }
 
-variable "create_option" {
-  type        = string
-  description = "The method to use when creating the managed disk"
-  default     = "Empty"
-}
+# variable "create_option" {
+#   type        = string
+#   description = "The method to use when creating the managed disk"
+#   default     = "Empty"
+# }
 
-variable "disk_size_gb" {
-  type        = string
-  description = "Required for a new managed disk) Specifies the size of the managed disk to create in gigabytes"
-}
+# variable "disk_size_gb" {
+#   type        = string
+#   description = "Required for a new managed disk) Specifies the size of the managed disk to create in gigabytes"
+# }
 
-variable "name" {
+variable "disk_id" {
   type        = string
   description = "The name of the disk"
 }
