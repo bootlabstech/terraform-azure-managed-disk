b5f98b2e114f580a8f4b1ba198f99f7b0d75659f
lalita2407 <lalitalallu02@gmail.com>
2023-09-18 13:44:56 +0530

    changed provider version 3.22 to 3.64

--- variables.tf
+++ variables.tf
@@ -4,28 +4,23 @@ variable "name" {
   description = "Specifies the name of the Managed Disk."
 
 }
-
 variable "location" {
   type        = string
   description = " Specified the supported Azure location where the resource exists. Changing this forces a new resource to be created"
 }
-
 variable "resource_group_name" {
   type        = string
   description = "The name of the Resource Group where the Managed Disk should exist"
 }
-
 variable "storage_account_type" {
   type        = string
   description = "The type of storage to use for the managed disk. Possible values are Standard_LRS, StandardSSD_ZRS, Premium_LRS, PremiumV2_LRS, Premium_ZRS, StandardSSD_LRS or UltraSSD_LRS"
 }
-
 variable "create_option" {
   type        = string
   description = "The method to use when creating the managed disk. Possible values include Empty, Import, ImportSecure, Copy, FromImage, Restore, Upload"
   default     = "Empty"
 }
-
 variable "disk_size_gb" {
   type        = string
   description = " Specifies the size of the managed disk to create in gigabytes. If create_option is Copy or FromImage, then the value must be equal to or greater than the source's size. The size can only be increased."
@@ -41,7 +36,6 @@ variable "lun" {
   description = "The Logical Unit Number of the Data Disk, which needs to be unique within the Virtual Machine."
   default     = "10"
 }
-
 variable "caching" {
   type        = string
   description = "Specifies the caching requirements for this Data Disk. Possible values include None, ReadOnly and ReadWrite."
